0ca76da0e813a78b42b7b733698864583300b053
Wayne Warren <waynr+launchpad@sdf.org>
2017-06-13 09:35:58 -0700

    Remove stub tests from test_pyresume module.

--- tests/test_pyresume.py
+++ tests/test_pyresume.py
@@ -8,29 +8,11 @@ test_pyresume
 Tests for `pyresume` module.
 """
 
-import pytest
-
-from contextlib import contextmanager
 from click.testing import CliRunner
 
-from pyresume import pyresume
 from pyresume import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
