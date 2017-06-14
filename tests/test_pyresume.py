0487ba030c600f2542d1f0a50dc403609b6897f7
Wayne Warren <wayne.warren.s@gmail.com>
2017-06-14 03:23:59 -0700

    Add CLI test to validate no yaml paths.

--- tests/test_pyresume.py
+++ tests/test_pyresume.py
@@ -21,3 +21,14 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_create_tex_no_yaml_paths():
+    """ Validate the appropriate exception is raised when no "yaml_paths"
+    arguments are passed to "pyresume create tex".
+    """
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ["create", "tex"])
+
+    assert "Must provide at least one yaml path." in result.output
+    assert result.exit_code == 2
