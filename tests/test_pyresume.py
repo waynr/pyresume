00df60a3aa28be87180a4b1ae1c92f552ebe6425
Wayne Warren <wayne.warren.s@gmail.com>
2020-09-20 20:27:49 -0500

    format code using black

--- tests/test_pyresume.py
+++ tests/test_pyresume.py
@@ -17,14 +17,14 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'pyresume' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "pyresume" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
 
 
 def test_create_tex_no_yaml_paths():
-    """ Validate the appropriate exception is raised when no "yaml_paths"
+    """Validate the appropriate exception is raised when no "yaml_paths"
     arguments are passed to "pyresume create tex".
     """
     runner = CliRunner()
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -15,19 +15,19 @@ from jinja2 import Environment
 
 def test_standard_template_exists():
     """
-    Validate that the standard LaTeX template is available through pkg_resources
-    at the expected location.
+    Validate that the standard LaTeX template is available through
+    pkg_resources at the expected location.
     """
-    assert pkg_resources.resource_exists('pyresume',
-                                         'templates/standard.tex')
+    assert pkg_resources.resource_exists("pyresume", "templates/standard.tex")
 
 
 def test_standard_template_is_valid_jinja2_format():
     """
-    Validate that the standard LaTeX template is well-formed as far as Jinja2 is
-    concerned.
+    Validate that the standard LaTeX template is well-formed as far as Jinja2
+    is concerned.
     """
     env = Environment()
-    template_string = pkg_resources.resource_string('pyresume',
-                                                    'templates/standard.tex').decode()
+    template_string = pkg_resources.resource_string(
+        "pyresume", "templates/standard.tex"
+    ).decode()
     env.parse(template_string)
